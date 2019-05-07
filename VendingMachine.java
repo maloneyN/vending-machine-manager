473bf7bc4348150942df1a3362f37163503bb769
maloneyN <nigelmaloney3@gmail.com>
2019-05-07 13:31:40 -0500

    Delete VendingMachine.java

--- VendingMachine.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public interface VendingMachine {
-	public long selectItemAndGetPrice (Item item);
-	public void insertCoin ( Coin coin);
-	public List <Coin> refund();
-	public Bucket<Item, List<Coin>> collectItemAndChange();
-	public void reset();
-}
