04d7a2052f43d7f2147c3127a67c7e22bd00c28c
maloneyN <nigelmaloney3@gmail.com>
2019-05-08 11:23:38 -0500

    Add files via upload
    I made new classes for the different vending machines and made hashes for the other food items. I started editing the menu so that the user can navigate between viewing current machines, adding new machines, and testing the machines. Right now we only have testing. This upload is mainly for saving the work and showing progress. I plan on getting the menu to send you to the different vending machine classes soon.

---
+++ ComboMachine.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vendingmachine;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class ComboMachine {
+
+    private static int Balance = 0; //stores total balance
+    private static Map<String, Integer> itemInventory = new HashMap<String, Integer>(); //hashmap for items and its numbers
+    private static Map<String, Integer> coinInventory = new HashMap<String, Integer>(); //hashmap for coins and its numbers
+    private static VendingExample VM = new VendingExample();
+
+    public static void main(String args[]) {
+
+        initiateInventory(); // initiate with items and coins storage in the vending machine
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\t\t\tVending Machine.");
+
+        while (Balance == 0) {
+            inputCoins(sc, VM); // input coins
+        }
+
+        String sample = selectItems(sc, VM); // select items
+        int checkVendCond = 0;
+        if (!itemInventory.containsKey(sample)) { // for return and cancel case
+            checkVendCond = VM.choose(sample, Balance);
+        } else if (itemInventory.get(sample) == 0) { // Sold out items
+            System.out.println("Sold Out, Please insert coins and buy another item");
+            VendingMachine.main(null);
+        } else {
+            checkVendCond = VM.choose(sample, Balance);
+            if (checkVendCond == 2) {
+                System.out.println("Insert More Coins");
+                inputCoins(sc, VM);
+                sample = selectItems(sc, VM);
+                if (itemInventory.get(sample) == 0) {
+                    System.out.println("Sold Out, Please insert coins and buy the items");
+                    VendingMachine.main(null);
+                } else if (itemInventory.get(sample) == 1 || itemInventory.get(sample) == -1) {
+
+                } else {
+                    itemInventory.put(sample, itemInventory.get(sample) - 1);
+                    Balance = checkVendCond;
+                }
+            } else {
+                itemInventory.put(sample, itemInventory.get(sample) - 1);
+                Balance = checkVendCond;
+            }
+
+        }
+
+        sc.close();
+    }
+
+    // insertion of coins
+    public static void inputCoins(Scanner sc, VendingExample VM) {
+        System.out.println("Insert coins in the form of: NICKLE, DIME, QUARTER and DOLLAR");
+        String coins = sc.nextLine();
+        Balance = VM.drop(coins, Balance);
+    }
+
+    //selection of items
+    public static String selectItems(Scanner sc, VendingExample VM) {
+        System.out.println("Your balance is " + Balance);
+        System.out.println("Select Item: \n");
+        System.out.println("COKE: Cola(1 dollar)");
+        System.out.println("CHIPS: Chips(50)");
+        System.out.println("CANDY: Candy(65)");
+        System.out.println("RETURN");
+        System.out.println("CANCEL");
+        return (sc.nextLine());
+    }
+
+    //initiate coins and items storage in vending machine
+    public static void initiateInventory() {
+        for (Item i : Item.values()) {
+            itemInventory.put(i.getName(), 5);
+        }
+        for (Coin i : Coin.values()) {
+            coinInventory.put(i.getName(), 5);
+        }
+    }
+
+    public static void changeBalance(int balance) {
+
+    }
+}
--- Item.java
+++ Item.java
@@ -1,17 +1,47 @@
-package finalproject;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vendingmachine;
 
-//Item
-public enum Item{ 
-	COKE("COKE", 100), GUSHERS("GUSHERS", 65), DORITOS("DORITOS", 50); 
-	private String name; 
-	private int price; 
-	private Item(String name, int price){ 
-		this.name = name; this.price = price; 
-	} 
-	public String getName(){ 
-		return name; 
-	} 
-	public int getPrice(){ 
-		return price; 
-	} 	
+public enum Item {
+
+    COKE("COKE", 100), 
+    SPRITE("SPRITE", 100), 
+    DR_PEPPER("DR.PEPPER", 100), 
+    FANTA("FANTA", 100), 
+    PIBB("PIBB", 100), 
+    LEMONADE("LEMONADE", 100),
+    ROOTBEER("ROOTBEER", 100),
+    CRUSH("CRUSH", 100),
+    LAYS("LAYS", 50),
+    TRISCUIT("TRISCUIT", 50),
+    SNYDERS("SNYDERS", 50),
+    SNICKERS("SNICKERS", 65),
+    SKITTLES("SKITTLES", 65),
+    LIFESAVERS("LIFESAVERS", 50),
+    FRITOS("FRITOS", 50),
+    DORITOS("DORITOS", 50),
+    SUNCHIPS("SUNCHIPS", 50),
+    RICEKRISPIES("RICEKRISPIES", 50),
+    //CANDY("CANDY", 65), 
+    //CHIPS("CHIPS", 50),
+    ;
+    
+    private String name;
+    private int price;
+
+    private Item(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
 }
