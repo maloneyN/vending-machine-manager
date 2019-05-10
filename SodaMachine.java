c9580c477af04cee62bfa70ac1fc282ab962c748
massiboffa <47925693+massiboffa@users.noreply.github.com>
2019-05-10 11:51:24 -0500

    Update SodaMachine.java

--- SodaMachine.java
+++ SodaMachine.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//author Massimo Boffa
 package finalproject;
 
 import java.util.HashMap;
@@ -22,13 +18,13 @@ class SodaMachine {
 
         initiateInventory(); // initiate with items and coins storage in the vending machine
 
-        Scanner sc = new Scanner(System.in);
+        Scanner user_input = new Scanner(System.in);
 
         while (Balance == 0) {
-            inputCoins(sc, VM); // input coins
+            inputCoins(user_input, VM); // input coins
         }
 
-        String sample = selectItems(sc, VM); // select items
+        String sample = selectItems(user_input, VM); // select items
         int checkVendCond = 0;
         if (!itemInventory.containsKey(sample)) { // for return and cancel case
             checkVendCond = VM.choose(sample, Balance);
@@ -39,8 +35,8 @@ class SodaMachine {
             checkVendCond = VM.choose(sample, Balance);
             if (checkVendCond == 2) {
                 System.out.println("Insert More Coins");
-                inputCoins(sc, VM);
-                sample = selectItems(sc, VM);
+                inputCoins(user_input, VM);
+                sample = selectItems(user_input, VM);
                 if (itemInventory.get(sample) == 0) {
                     System.out.println("Sold Out, Please insert coins and buy the items");
                     VendingMachine.main(null);
@@ -57,18 +53,18 @@ class SodaMachine {
 
         }
 
-        sc.close();
+        user_input.close();
     }
 
     // insertion of coins
-    public static void inputCoins(Scanner sc, VendingExample VM) {
+    public static void inputCoins(Scanner user_input, VendingExample VM) {
         System.out.println("\nInsert coins in the form of: NICKEL, DIME, QUARTER and DOLLAR");
-        String coins = sc.nextLine();
+        String coins = user_input.nextLine();
         Balance = VM.drop(coins, Balance);
     }
 
     //selection of items
-    public static String selectItems(Scanner sc, VendingExample VM) {
+    public static String selectItems(Scanner user_input, VendingExample VM) {
         System.out.println("Your balance is " + Balance);
         System.out.println("Select Item: \n");
         System.out.println("COKE: Cola(1 dollar)");
@@ -81,7 +77,7 @@ class SodaMachine {
         System.out.println("CRUSH: Crush(1 dollar)");
         System.out.println("RETURN");
         System.out.println("CANCEL");
-        return (sc.nextLine());
+        return (user_input.nextLine());
     }
 
     //initiate coins and items storage in vending machine
